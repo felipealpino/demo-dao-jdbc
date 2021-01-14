b52d0ab1b43f61a58fec4c17726d34ce51827489
Felipe Alpino <felipealpino1@gmail.com>
2021-01-13 23:56:49 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("=== TESTE 1: seller findById ===");
@@ -39,12 +41,22 @@ public class Program {
 		
 		System.out.println();
 		System.out.println("=== TESTE 5: seller Update ===");
-		//department criado no teste 2
 		seller = sellerDao.findById(7); // pegando o item no id 7 -- Felipe Gontijo 
 		seller.setEmail("felipeSinerd@codingHard.com"); // Setando um diferente email para o objeto
 		sellerDao.update(seller); // fazendo o update do novo objeto 
 		System.out.println("Modificado! " + seller );
 		
+		
+		System.out.println();
+		System.out.println("=== TESTE 6: seller Delete ===");
+		System.out.print("Entre um ID para ser deletado: ");
+		int idToDelete = sc.nextInt();
+		sc.nextLine();
+		sellerDao.deleteById(idToDelete); // fazendo o update do novo objeto 
+		System.out.println("Deletado!");
+		
+		
+		sc.close();
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -93,7 +93,23 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		ResultSet rs = null;;
+		
+		try {
+			st = conn.prepareStatement(
+					"DELETE FROM seller "
+					+ "WHERE Id = ?");
+			
+			st.setInt(1, id);
+			st.executeUpdate();
+			
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 
 	}
 
